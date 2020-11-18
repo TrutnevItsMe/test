9c6413b075dd05e0c7e3028e5b3895a9a5f4c803
akentyev <akentyev@intervolga.ru>
2020-11-18 11:42:26 +0400

    [iberis-8] Отображение количества в комплекте

--- local/templates/.default/components/bitrix/catalog.element/main_set/set_composition.php
+++ local/templates/.default/components/bitrix/catalog.element/main_set/set_composition.php
@@ -15,76 +15,85 @@ if (is_array($arResult["SET"])):?>
 					data-price="<?= $arItem['PRICE'] ?>"
 					data-old-price="<?= isset($arItem['OLD_PRICE']) ? $arItem['OLD_PRICE'] : $arItem['PRICE'] ?>"
 				>
-			<span class="set-composition_checkbox">
-				<input type="checkbox" checked disabled>
-			</span>
-					<span class="set-composition_picture">
+				<span class="set-composition_checkbox">
+					<input type="checkbox" checked disabled>
+				</span>
+				<span class="set-composition_picture">
 				<? if ($arItem['PREVIEW_PICTURE']): ?>
 					<img src="<?= $arItem['PREVIEW_PICTURE'] ?>">
 				<? else: ?>
 					<img src="/bitrix/templates/aspro_next/images/no_photo_medium.png">
 				<? endif ?>
-			</span>
-					<span class="set-composition_title">
-				<div class="set-composition_name">
-					<a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
-					<? if (isset($arItem['PERCENT'])): ?>
-						<span class="set-composition-sticker"><?= $arItem['PERCENT'] ?>%</span>
-					<? endif ?>
-				</div>
-				<div class="set-composition_article">Артикул: <?= $arItem['ARTICLE'] ?></div>
-			</span>
-					<span class="set-composition_price">
+				</span>
+				<span class="set-composition_title">
+					<div class="set-composition_name">
+						<a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
+						<? if (isset($arItem['PERCENT'])): ?>
+							<span class="set-composition-sticker"><?= $arItem['PERCENT'] ?>%</span>
+						<? endif ?>
+					</div>
+					<div class="set-composition_article">Артикул: <?= $arItem['ARTICLE'] ?></div>
+					<?if ($arItem['AMOUNT'] > 1):?>
+						<div class="set-composition_amount">В комплект входит: <?= $arItem['AMOUNT'] ?> шт.</div>
+					<?endif?>
+				</span>
+				<span class="set-composition_price">
 				<? if (isset($arItem['OLD_PRICE'])): ?>
 					<div class="set-composition_price__old"><?=
+						($arItem['AMOUNT'] > 1) ? $arItem['AMOUNT'] . ' x ' : '' ?><?=
 						number_format(
-							$arItem['OLD_PRICE'] * $arItem['AMOUNT'],
+							$arItem['OLD_PRICE'],
 							2,
 							',',
 							'&nbsp;'
 						) ?> руб.</div>
 				<? endif ?>
-						<div class="set-composition_price__new"><?=
-							number_format(
-								$arItem['PRICE'] * $arItem['AMOUNT'],
-								2,
-								',',
-								'&nbsp;'
-							) ?> руб.</div>
-			</span>
-				</div>
+					<div class="set-composition_price__new"><?=
+						($arItem['AMOUNT'] > 1) ? $arItem['AMOUNT'] . ' x ' : '' ?><?=
+						number_format(
+							$arItem['PRICE'],
+							2,
+							',',
+							'&nbsp;'
+						) ?> руб.</div>
+				</span>
+			</div>
 			<? endforeach; ?>
 		</div>
 		<? if (count($arResult["SET"]["OPTIONAL"]) > 0): ?>
-			<div class="set-composition_accesories">
-				<h4 class="set-composition_header">Комплектующие (<?= count($arResult["SET"]["OPTIONAL"]) ?>)</h4>
-				<? foreach ($arResult["SET"]["OPTIONAL"] as $arItem): ?>
-					<div
-						class="set-composition_row"
-						data-id="<?= $arItem['ID'] ?>"
-						data-amount="<?= $arItem['AMOUNT'] ?>"
-						data-price="<?= $arItem['PRICE'] ?>"
-						data-old-price="<?= isset($arItem['OLD_PRICE']) ? $arItem['OLD_PRICE'] : $arItem['PRICE'] ?>"
-					>
-			<span class="set-composition_checkbox">
-				<input type="checkbox"<?= $arItem['DEFAULT'] ? ' checked' : '' ?>>
-			</span>
-						<span class="set-composition_picture">
+		<div class="set-composition_accesories">
+			<h4 class="set-composition_header">Комплектующие (<?= count($arResult["SET"]["OPTIONAL"]) ?>)</h4>
+			<? foreach ($arResult["SET"]["OPTIONAL"] as $arItem): ?>
+			<div
+				class="set-composition_row"
+				data-id="<?= $arItem['ID'] ?>"
+				data-amount="<?= $arItem['AMOUNT'] ?>"
+				data-price="<?= $arItem['PRICE'] ?>"
+				data-old-price="<?= isset($arItem['OLD_PRICE']) ? $arItem['OLD_PRICE'] : $arItem['PRICE'] ?>"
+			>
+				<span class="set-composition_checkbox">
+					<input type="checkbox"<?= $arItem['DEFAULT'] ? ' checked' : '' ?>>
+				</span>
+				<span class="set-composition_picture">
 				<? if ($arItem['PREVIEW_PICTURE']): ?>
 					<img src="<?= $arItem['PREVIEW_PICTURE'] ?>">
 				<? else: ?>
 					<img src="/bitrix/templates/aspro_next/images/no_photo_medium.png">
 				<? endif ?>
-			</span>
-						<span class="set-composition_title">
-				<div class="set-composition_name">
-					<a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
-				</div>
-				<div class="set-composition_article">Артикул: <?= $arItem['ARTICLE'] ?></div>
-			</span>
-						<span class="set-composition_price">
+				</span>
+				<span class="set-composition_title">
+					<div class="set-composition_name">
+						<a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
+					</div>
+					<div class="set-composition_article">Артикул: <?= $arItem['ARTICLE'] ?></div>
+					<?if ($arItem['AMOUNT'] > 1):?>
+						<div class="set-composition_amount">В набор входит: <?= $arItem['AMOUNT'] ?> шт.</div>
+					<?endif?>
+				</span>
+				<span class="set-composition_price">
 				<? if (isset($arItem['OLD_PRICE'])): ?>
 					<div class="set-composition_price__old"><?=
+						($arItem['AMOUNT'] > 1) ? $arItem['AMOUNT'] . ' x ' : '' ?><?=
 						number_format(
 							$arItem['OLD_PRICE'] * $arItem['AMOUNT'],
 							2,
@@ -92,17 +101,18 @@ if (is_array($arResult["SET"])):?>
 							'&nbsp;'
 						) ?> руб.</div>
 				<? endif ?>
-							<div class="set-composition_price__new"><?=
-								number_format(
-									$arItem['PRICE'] * $arItem['AMOUNT'],
-									2,
-									',',
-									'&nbsp;'
-								) ?> руб.</div>
-			</span>
-					</div>
-				<? endforeach; ?>
+					<div class="set-composition_price__new"><?=
+						($arItem['AMOUNT'] > 1) ? $arItem['AMOUNT'] . ' x ' : '' ?><?=
+						number_format(
+							$arItem['PRICE'],
+							2,
+							',',
+							'&nbsp;'
+						) ?> руб.</div>
+				</span>
 			</div>
+			<? endforeach; ?>
+		</div>
 		<? endif ?>
 	</div>
 <? endif;
