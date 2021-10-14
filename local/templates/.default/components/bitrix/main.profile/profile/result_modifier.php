d8000018a9ff4c3e0e9d7e6e4102aa81576b3db9
akentyev <akentyev@intervolga.ru>
2021-10-14 09:27:49 +0300

    [iberis-13] Убрали двух помошников из выборки

--- local/templates/.default/components/bitrix/main.profile/profile/result_modifier.php
+++ local/templates/.default/components/bitrix/main.profile/profile/result_modifier.php
@@ -8,7 +8,7 @@ use Intervolga\Common\Highloadblock\HlbWrap;
 $hl = new HlbWrap('Partnery');
 $partner = $hl->getList([
 	'filter' => ['=UF_XML_ID' => $arResult['arUser']["XML_ID"]],
-	'select' => ['UF_OSNOVNOYMENEDZHER', 'UF_POMOSHNIK1', 'UF_POMOSHNIK2', 'UF_POMOSHNIK3', 'UF_POMOSHNIK4',],
+	'select' => ['UF_OSNOVNOYMENEDZHER', 'UF_POMOSHNIK1', 'UF_POMOSHNIK2', /*'UF_POMOSHNIK3', 'UF_POMOSHNIK4',*/],
 ])->fetch();
 if ($partner
 	&& ($partner['UF_OSNOVNOYMENEDZHER'] || $partner['UF_POMOSHNIK1'] || $partner['UF_POMOSHNIK2']
