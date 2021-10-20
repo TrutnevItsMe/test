86d3d19c10e7ffff67411ce4f907d1bb775cc4e2
akentyev <akentyev@intervolga.ru>
2021-10-20 12:17:13 +0300

    [iberis-14] Проверки, сообщения об ошибках

--- ajax/getCustomPrices.php
+++ ajax/getCustomPrices.php
@@ -2,16 +2,34 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 use Bitrix\Main\Web\Json;
 use Intervolga\Custom\Import\CustomPrices;
-
-$prices = CustomPrices::get(
-	$_POST['userXmlId'],
-	$_POST['counterpartyXmlId'],
-	$_POST['agreementXmlId'],
-	$_POST['basket']
+$error = '';
+if (!check_bitrix_sessid()) {
+	$error = 'Неверный sessid';
+}
+if (!$error
+	&& trim($_POST['userXmlId'])
+	&& trim($_POST['counterpartyXmlId'])
+	&& trim($_POST['agreementXmlId'])
+	&& is_array($_POST['basket'])
+) {
+	try {
+		$prices = CustomPrices::get(
+			$_POST['userXmlId'],
+			$_POST['counterpartyXmlId'],
+			$_POST['agreementXmlId'],
+			$_POST['basket']
+		);
+		CustomPrices::set($prices, $_POST['basket']);
+	} catch (Error $err) {
+		// Произошла ошибка исполнения
+		$error = 'Произошла ошибка при получении цен';
+	}
+} else {
+	$error = 'Неверные входные данные';
+}
+header("Content-type: application/json; charset=utf-8");
+echo Json::encode(
+	$error ? ['result' => 'error', 'data' => $error] : ['result' => 'ok', 'data' => $prices]
 );
-CustomPrices::set($prices, $_POST['basket']);
-header('Content-type: application/json');
-echo Json::encode(['result' => 'ok', 'data' => $prices]);
-
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");
