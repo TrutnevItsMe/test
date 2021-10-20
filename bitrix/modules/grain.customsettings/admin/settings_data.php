4def493ff7c833f68f43555e2a8d8bdcc46467b4
akentyev <akentyev@intervolga.ru>
2021-10-20 12:56:53 +0300

    [iberis-14] Переход на "Дополнительные настройки"

---
+++ bitrix/modules/grain.customsettings/admin/settings_data.php
@@ -0,0 +1,65 @@
+<?
+
+$arCustomPage = array (
+  'PARENT_MENU' => 'global_menu_settings',
+  'SORT' => '1780',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'MENU_TEXT' => 'Дополнительные настройки',
+      'MENU_TITLE' => 'Дополнительные настройки',
+      'PAGE_TITLE' => 'Дополнительные настройки',
+    ),
+    'en' => 
+    array (
+      'MENU_TEXT' => 'Additional settings',
+      'MENU_TITLE' => 'Additional settings',
+      'PAGE_TITLE' => 'Additional settings',
+    ),
+  ),
+);
+
+$arCustomSettings = array (
+  1 => 
+  array (
+    'LANG' => 
+    array (
+      'ru' => 
+      array (
+        'NAME' => 'Главная',
+        'TITLE' => 'Основные настройки',
+      ),
+      'en' => 
+      array (
+        'NAME' => '',
+        'TITLE' => '',
+      ),
+    ),
+    'FIELDS' => 
+    array (
+      1 => 
+      array (
+        'NAME' => 'custom_prices_url',
+        'LANG' => 
+        array (
+          'ru' => 
+          array (
+            'NAME' => 'Ссылка для API персональных цен',
+            'TOOLTIP' => '',
+          ),
+          'en' => 
+          array (
+            'NAME' => '',
+            'TOOLTIP' => '',
+          ),
+        ),
+        'TYPE' => 'text',
+        'DEFAULT_VALUE' => '',
+        'SIZE' => '100',
+      ),
+    ),
+  ),
+);
+
+?>
