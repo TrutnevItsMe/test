edbfd964049ca8ff14705beb1e474baf320f9aa5
Иван Трутнев <trutnev@intervolga.ru>
2022-05-17 17:52:39 +0300

    [iberisweb-54] Сделать форму регистрации, которая будет уходить на почту менеджеру Создан обработчик события, который отправляет результаты всем пользователям в группе менеджеров

---
+++ local/modules/intervolga.custom/lib/eventhandlers/form.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Intervolga\Custom\EventHandlers;
+
+use Intervolga\Common\Tools\EventHandler;
+use Intervolga\Custom\Import\Sets;
+
+class Form extends EventHandler {
+
+	const REGISTER_WEB_FORM_ID = 10;
+
+	public static function onAfterResultAdd($WEB_FORM_ID, $RESULT_ID){
+
+		if (\Bitrix\Main\Loader::IncludeModule("form") && $WEB_FORM_ID == static::REGISTER_WEB_FORM_ID){
+
+			$arResult = [];
+			$arAnswer2 = [];
+
+			$webFormResult = $arAnswer = \CFormResult::GetDataByID(
+				$RESULT_ID,
+				array(),
+				$arResult,
+				$arAnswer2);
+
+			$managerList = \CUser::GetList(
+				$by = "ID",
+				 $order = "asc",
+				$filter = array(
+					"GROUPS_ID" => [MANAGER_GROUP_ID],
+					"ACTIVE" => "Y"
+				));
+
+			$arManagersEmail = [];
+
+			while ($manager = $managerList->GetNext()){
+				$arManagersEmail[] = $manager["EMAIL"];
+			}
+
+			if ($webFormResult["SEX"][0]["ANSWER_VALUE"] == "MALE"){
+				$sex = "мужской";
+			}
+			elseif ($webFormResult["SEX"][0]["ANSWER_VALUE"] == "FEMALE"){
+				$sex = "женский";
+			}
+
+			$arEventFields = [
+				"MANAGER_EMAIL" => implode(',', $arManagersEmail),
+				"ANSWER_SURNAME" => $webFormResult["SURNAME"][0]["USER_TEXT"],
+				"ANSWER_NAME" => $webFormResult["NAME"][0]["USER_TEXT"],
+				"ANSWER_PATRONYMIC" => $webFormResult["PATRONYMIC"][0]["USER_TEXT"],
+				"ANSWER_EMAIL" => $webFormResult["EMAIL"][0]["USER_TEXT"],
+				"ANSWER_LOGIN" => $webFormResult["LOGIN"][0]["USER_TEXT"],
+				"ANSWER_PASSWORD" => $webFormResult["PASSWORD"][0]["USER_TEXT"],
+				"ANSWER_PHONE" => $webFormResult["PHONE"][0]["USER_TEXT"],
+				"ANSWER_BIRTHDATE" => $webFormResult["BIRTHDATE"][0]["USER_DATE"],
+				"ANSWER_SEX" => $sex
+			];
+
+			\CEvent::Send("REGISTER_FORM_COMPLETED",
+			"s1",
+				$arEventFields
+			);
+		}
+	}
+}
---
+++ test.php
@@ -0,0 +1,26 @@
+<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+
+$APPLICATION->SetTitle(""); ?><?$APPLICATION->IncludeComponent(
+	"bitrix:form.result.new", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"WEB_FORM_ID" => "10",
+		"IGNORE_CUSTOM_TEMPLATE" => "N",
+		"USE_EXTENDED_ERRORS" => "N",
+		"SEF_MODE" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600",
+		"LIST_URL" => "result_list.php",
+		"EDIT_URL" => "result_edit.php",
+		"SUCCESS_URL" => "",
+		"CHAIN_ITEM_TEXT" => "",
+		"CHAIN_ITEM_LINK" => "",
+		"VARIABLE_ALIASES" => array(
+			"WEB_FORM_ID" => "WEB_FORM_ID",
+			"RESULT_ID" => "RESULT_ID",
+		)
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
