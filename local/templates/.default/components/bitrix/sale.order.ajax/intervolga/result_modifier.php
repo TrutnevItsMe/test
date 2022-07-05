4d193e96bebd4373c42bd0a18f4302581507218d
Иван Трутнев <trutnev@intervolga.ru>
2022-07-05 10:21:59 +0300

    [iberisweb-67] Проверить работу кнопки получения индивидуальных цен Вычислены скидки

--- local/templates/.default/components/bitrix/sale.order.ajax/intervolga/result_modifier.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/intervolga/result_modifier.php
@@ -5,6 +5,7 @@ use CSaleOrderUserProps;
 use Bitrix\Main\UserTable;
 use Intervolga\Custom\Import\Sets;
 use Intervolga\Common\Highloadblock\HlbWrap;
+use Intervolga\Custom\Tools\HighloadblockUtil;
 
 /**
  * @var array $arParams
@@ -15,6 +16,18 @@ use Intervolga\Common\Highloadblock\HlbWrap;
 $component = $this->__component;
 $component::scaleImages($arResult['JS_DATA'], $arParams['SERVICES_IMAGES_SCALING']);
 
+
+/*
+ *
+ * Вычисление скидки
+ * */
+foreach ($arResult["BASKET_ITEMS"] as $ID => $arRow){
+	$oldPrice = $arResult["BASKET_ITEMS"][$ID]["SUM_BASE"];
+	$newPrice = $arResult["BASKET_ITEMS"][$ID]["SUM_NUM"];
+	$arResult["BASKET_ITEMS"][$ID]["DISCOUNT_PRICE_PERCENT"] = 1 - $newPrice / $oldPrice;
+	$arResult["BASKET_ITEMS"][$ID]["DISCOUNT_PRICE_PERCENT_FORMATED"] = round($arResult["BASKET_ITEMS"][$ID]["DISCOUNT_PRICE_PERCENT"] * 100, 2) . "%";
+}
+
 // https://youtrack.ivsupport.ru/issue/iberisweb-8
 if (is_array($arResult["GRID"]["ROWS"])) {
 	$catalogIblockID = Option::get(
@@ -23,6 +36,16 @@ if (is_array($arResult["GRID"]["ROWS"])) {
 		CNextCache::$arIBlocks[SITE_ID]['aspro_next_catalog']['aspro_next_catalog'][0]
 	);
 	foreach ($arResult['JS_DATA']["GRID"]["ROWS"] as $key => $arItem) {
+
+		/*
+		 *
+ 		* Вычисление скидки
+ 		* */
+		$oldPrice = $arResult['JS_DATA']["GRID"]["ROWS"][$key]["data"]["SUM_BASE"];
+		$newPrice = $arResult['JS_DATA']["GRID"]["ROWS"][$key]["data"]["SUM_NUM"];
+		$arResult['JS_DATA']["GRID"]["ROWS"][$key]["data"]["DISCOUNT_PRICE_PERCENT"] = 1 - $newPrice / $oldPrice;
+		$arResult['JS_DATA']["GRID"]["ROWS"][$key]["data"]["DISCOUNT_PRICE_PERCENT_FORMATED"] = round($arResult['JS_DATA']["GRID"]["ROWS"][$key]["data"]["DISCOUNT_PRICE_PERCENT"] * 100, 2) . "%";
+
 		$productId = $arItem["data"]["PRODUCT_ID"];
 		$rsProperty = CIBlockElement::GetProperty(
 			$catalogIblockID,
@@ -55,10 +78,12 @@ while ($profile = $dbProfiles -> Fetch()) {
 		$profiles[$profile['ID']] = $profile;
 	}
 }
+
 $user = UserTable::getRow([
 	'filter' => ['=ID' => (reset($profiles))['USER_ID']],
 	'select' => ['ID', 'XML_ID']
 ]);
+
 if ($user) {
 	$userXmlId = $user['XML_ID'];
 }
@@ -102,3 +127,12 @@ $arResult['PARTNERS'] = [
 	'basket' => $basketItems,
 ];
 
+
+$rsUser = UserTable::GetByID($USER->GetID());
+$arUser = $rsUser->fetch();
+
+$idPartneryHL = HighloadblockUtil::getHLBlockIdByCode(HL_BLOCK_CODE_PARTNERY);
+$dbPartnery = HighloadblockUtil::getList($idPartneryHL, ["UF_XML_ID" => $arUser["XML_ID"]]);
+
+$partner = $dbPartnery->fetch();
+$arResult["PARTNER"] = $partner;
