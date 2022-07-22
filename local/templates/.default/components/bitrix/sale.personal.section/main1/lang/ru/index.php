68d3308105e95a62ebaa534ac0a218f034bf1341
Иван Трутнев <trutnev@intervolga.ru>
2022-07-22 10:48:02 +0300

    [iberisweb-107] Текущие заказы переименовать в предварительные Изменен lang файл

--- local/templates/.default/components/bitrix/sale.personal.section/main1/lang/ru/index.php
+++ local/templates/.default/components/bitrix/sale.personal.section/main1/lang/ru/index.php
@@ -2,7 +2,7 @@
 $MESS["SPS_ACCOUNT_PAGE_NAME"] = "Личный счет";
 $MESS["SPS_PERSONAL_PAGE_NAME"] = "Личные данные";
 $MESS["SPS_PROFILE_PAGE_NAME"] = "Профили заказов";
-$MESS["SPS_ORDER_PAGE_NAME"] = "Текущие заказы";
+$MESS["SPS_ORDER_PAGE_NAME"] = "Предварительные заказы";
 $MESS["SPS_ORDER_PAGE_HISTORY"] = "История заказов";
 $MESS["SPS_SUBSCRIBE_PAGE_NAME"] = "Подписки";
 $MESS["SPS_BASKET_PAGE_NAME"] = "Корзина";
