1be38e9946ebf5e52ec6dc765693d151bf277ace
Иван Трутнев <trutnev@intervolga.ru>
2022-08-08 17:22:19 +0300

    [iberisweb-98] Убраны теги <hr> и изменена ширина блока в оформлении заказа

--- local/components/intervolga/managersByUser/templates/.default/template.php
+++ local/components/intervolga/managersByUser/templates/.default/template.php
@@ -1,22 +1,16 @@
 <? if ($arResult["PARTNERS"]): ?>
 
-<style>
-    hr{
-        width: 100%;
-    }
-</style>
+	<table class="contact-table">
+		<thead>
+		<tr>
+			<td><?= \Bitrix\Main\Localization\Loc::getMessage("PARTNER") ?></td>
+			<td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER") ?></td>
+			<td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER1") ?></td>
+			<td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER2") ?></td>
+		</tr>
+		</thead>
 
-    <table class="contact-table">
-        <thead>
-        <tr>
-            <td><?= \Bitrix\Main\Localization\Loc::getMessage("PARTNER") ?><hr></td>
-            <td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER") ?><hr></td>
-            <td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER1") ?><hr></td>
-            <td><?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER2") ?><hr></td>
-        </tr>
-        </thead>
-
-        <tbody>
+		<tbody>
 
 		<?
 		$countPartners = 0;
@@ -24,42 +18,38 @@
 
 		<? foreach ($arResult["PARTNERS"] as $partner): ?>
 
-            <tr>
-                <td><?= $partner["UF_NAME"] ?></td>
-                <td><?= $partner["UF_OSNOVNOYMENEDZHER"] ?></td>
-                <td><?= $partner["UF_POMOSHNIK1"] ?></td>
-                <td><?= $partner["UF_POMOSHNIK2"] ?></td>
-            </tr>
+			<tr>
+				<td><?= $partner["UF_NAME"] ?></td>
+				<td><?= $partner["UF_OSNOVNOYMENEDZHER"] ?></td>
+				<td><?= $partner["UF_POMOSHNIK1"] ?></td>
+				<td><?= $partner["UF_POMOSHNIK2"] ?></td>
+			</tr>
 
-            <tr>
-                <td>
-                    <a href="mailto:<?= $partner["UF_IMLOGIN"] ?>"><?= $partner["UF_IMLOGIN"] ?></a>
-                    <hr>
-                </td>
-                <td>
-                    <a href="tel:<?= $partner["UF_OSNMENEDZHERTELEF"] ?>"><?= $partner["DISPLAY_OSNMENEDZHERTELEF"] ?></a>
-                    <a href="mailto:<?= $partner["UF_OSNMENEDZHERADRES"] ?>"><?= $partner["UF_OSNMENEDZHERADRES"] ?></a>
-                    <hr>
-                </td>
-                <td>
-                    <a href="tel:<?= $partner["UF_POMOSHNIKTELEFON1"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON1"] ?></a>
-                    <a href="mailto:<?= $partner["UF_POMOSHNIKADRES1"] ?>"><?= $partner["UF_POMOSHNIKADRES1"] ?></a>
-                    <hr>
-                </td>
-                <td>
-                    <a href="tel:<?= $partner["UF_POMOSHNIKTELEFON2"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON2"] ?></a>
-                    <a href="mailto:<?= $partner["UF_POMOSHNIKADRES2"] ?>"><?= $partner["UF_POMOSHNIKADRES2"] ?></a>
-                    <hr>
-                </td>
-            </tr>
+			<tr>
+				<td>
+					<a href="mailto:<?= $partner["UF_IMLOGIN"] ?>"><?= $partner["UF_IMLOGIN"] ?></a>
+				</td>
+				<td>
+					<a href="tel:<?= $partner["UF_OSNMENEDZHERTELEF"] ?>"><?= $partner["DISPLAY_OSNMENEDZHERTELEF"] ?></a>
+					<a href="mailto:<?= $partner["UF_OSNMENEDZHERADRES"] ?>"><?= $partner["UF_OSNMENEDZHERADRES"] ?></a>
+				</td>
+				<td>
+					<a href="tel:<?= $partner["UF_POMOSHNIKTELEFON1"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON1"] ?></a>
+					<a href="mailto:<?= $partner["UF_POMOSHNIKADRES1"] ?>"><?= $partner["UF_POMOSHNIKADRES1"] ?></a>
+				</td>
+				<td>
+					<a href="tel:<?= $partner["UF_POMOSHNIKTELEFON2"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON2"] ?></a>
+					<a href="mailto:<?= $partner["UF_POMOSHNIKADRES2"] ?>"><?= $partner["UF_POMOSHNIKADRES2"] ?></a>
+				</td>
+			</tr>
 
 
 			<? ++$countPartners; ?>
 		<? endforeach; ?>
 
 
-        </tbody>
-    </table>
+		</tbody>
+	</table>
 <? endif; ?>
 
 
--- local/modules/intervolga.custom/lib/eventhandlers/iblock.php
+++ local/modules/intervolga.custom/lib/eventhandlers/iblock.php
@@ -6,7 +6,7 @@ class Iblock extends EventHandler{
 
 	public static function OnBeforeIBlockSectionUpdate(&$arParams){
 
-		// Отменить изменение названий разделов, если они передаын из 1С
+		// Отменить изменение названий разделов, если они переданы из 1С
 		if ($_REQUEST["mode"] == "import"){
 			unset($arParams["NAME"]);
 		}
