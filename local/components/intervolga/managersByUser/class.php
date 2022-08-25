dc5471b958fd5c721efe337da74a4bb4c1b955af
Иван Трутнев <trutnev@intervolga.ru>
2022-08-25 13:58:38 +0300

    [iberisweb-122] Добавил название столбцов для остатков и склада

--- local/components/intervolga/managersByUser/class.php
+++ local/components/intervolga/managersByUser/class.php
@@ -7,7 +7,7 @@ use Intervolga\Common\Highloadblock\HlbWrap;
 
 class ManagersByUser extends CBitrixComponent{
 
-	protected function fillArResult(){
+	protected function fillResult(){
 
 		global $USER;
 
@@ -21,6 +21,9 @@ class ManagersByUser extends CBitrixComponent{
 				$partner["DISPLAY_OSNMENEDZHERTELEF"] = Parser::getInstance()->parse($partner["UF_OSNMENEDZHERTELEF"])->format(Format::NATIONAL);
 				$partner["DISPLAY_POMOSHNIKTELEFON1"] = Parser::getInstance()->parse($partner["UF_POMOSHNIKTELEFON1"])->format(Format::NATIONAL);
 				$partner["DISPLAY_POMOSHNIKTELEFON2"] = Parser::getInstance()->parse($partner["UF_POMOSHNIKTELEFON2"])->format(Format::NATIONAL);
+				$partner["DISPLAY_POMOSHNIKTELEFON3"] = Parser::getInstance()->parse($partner["UF_POMOSHNIKTELEFON3"])->format(Format::NATIONAL);
+				$partner["DISPLAY_OSNMENEDZHERRABTE"] = Parser::getInstance()->parse($partner["UF_OSNMENEDZHERRABTE"])->format(Format::NATIONAL);
+				$partner["DISPLAY_POMOSHNIKRABTELEF"] = Parser::getInstance()->parse($partner["UF_POMOSHNIKRABTELEF"])->format(Format::NATIONAL);
 				$this->arResult["PARTNERS"][] = $partner;
 			}
 		}
@@ -28,7 +31,7 @@ class ManagersByUser extends CBitrixComponent{
 
 	public function executeComponent() {
 
-		self::fillArResult();
+		self::fillResult();
 		$this->includeComponentTemplate();
 	}
 }
