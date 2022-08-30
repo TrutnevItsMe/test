4ea0a714d2c9b87b915285efcec1b94a510dc5b4
Иван Трутнев <trutnev@intervolga.ru>
2022-08-30 17:33:46 +0300

    [iberisweb-123] убрал картинку товара из корзины

--- basket/index.php
+++ basket/index.php
@@ -2,9 +2,9 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Корзина");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:sale.basket.basket",
-	"intervolga",
-	Array(
+	"bitrix:sale.basket.basket", 
+	"intervolga", 
+	array(
 		"ACTION_VARIABLE" => "action",
 		"ADDITIONAL_PICT_PROP_17" => "-",
 		"ADDITIONAL_PICT_PROP_20" => "-",
@@ -12,16 +12,39 @@ $APPLICATION->SetTitle("Корзина");
 		"AUTO_CALCULATION" => "Y",
 		"BASKET_IMAGES_SCALING" => "adaptive",
 		"BASKET_WITH_ORDER_INTEGRATION" => "N",
-		"COLUMNS_LIST" => array(0=>"NAME",1=>"DISCOUNT",2=>"PROPS",3=>"DELETE",4=>"DELAY",5=>"TYPE",6=>"PRICE",7=>"QUANTITY",8=>"SUM",),
-		"COLUMNS_LIST_EXT" => array("PREVIEW_PICTURE","DELETE","DELAY","TYPE","SUM","PROPERTY_CML2_ARTICLE","PROPERTY_SKIDKA","PROPERTY_ARTICLE"),
-		"COLUMNS_LIST_MOBILE" => array("PREVIEW_PICTURE","DELETE","DELAY","TYPE","SUM"),
+		"COLUMNS_LIST" => array(
+			0 => "NAME",
+			1 => "DISCOUNT",
+			2 => "PROPS",
+			3 => "DELETE",
+			4 => "DELAY",
+			5 => "TYPE",
+			6 => "PRICE",
+			7 => "QUANTITY",
+			8 => "SUM",
+		),
+		"COLUMNS_LIST_EXT" => array(
+			0 => "DELETE",
+			1 => "DELAY",
+			2 => "TYPE",
+			3 => "SUM",
+			4 => "PROPERTY_CML2_ARTICLE",
+			5 => "PROPERTY_SKIDKA",
+			6 => "PROPERTY_ARTICLE",
+		),
+		"COLUMNS_LIST_MOBILE" => array(
+			0 => "DELETE",
+			1 => "DELAY",
+			2 => "TYPE",
+			3 => "SUM",
+		),
 		"COMPATIBLE_MODE" => "Y",
 		"COMPOSITE_FRAME_MODE" => "A",
 		"COMPOSITE_FRAME_TYPE" => "AUTO",
 		"CORRECT_RATIO" => "Y",
 		"COUNT_DISCOUNT_4_ALL_QUANTITY" => "N",
 		"DEFERRED_REFRESH" => "N",
-		"DEF_STORE_ID" => 376,
+		"DEF_STORE_ID" => "376",
 		"DISCOUNT_PERCENT_POSITION" => "bottom-right",
 		"DISPLAY_MODE" => "extended",
 		"DISPLAY_MODE_ITEMS" => "basket-item-table-props-template",
@@ -43,10 +66,12 @@ $APPLICATION->SetTitle("Корзина");
 		"GIFTS_SHOW_OLD_PRICE" => "Y",
 		"GIFTS_TEXT_LABEL_GIFT" => "Подарок",
 		"HIDE_COUPON" => "Y",
-		"LABEL_PROP" => array(),
-		"LABEL_PROP_MOBILE" => array(),
+		"LABEL_PROP" => array(
+		),
+		"LABEL_PROP_MOBILE" => "",
 		"LABEL_PROP_POSITION" => "top-left",
-		"OFFERS_PROPS" => array("SIZES","COLOR_REF"),
+		"OFFERS_PROPS" => array(
+		),
 		"PATH_TO_ORDER" => SITE_DIR."order/",
 		"PICTURE_HEIGHT" => "100",
 		"PICTURE_WIDTH" => "100",
@@ -65,28 +90,17 @@ $APPLICATION->SetTitle("Корзина");
 		"SHOW_RESTORE" => "Y",
 		"SHOW_STORE_NAME" => "Y",
 		"TEMPLATE_THEME" => "blue",
-		"TOTAL_BLOCK_DISPLAY" => array("top"),
+		"TOTAL_BLOCK_DISPLAY" => array(
+			0 => "top",
+		),
 		"USE_DYNAMIC_SCROLL" => "Y",
 		"USE_ENHANCED_ECOMMERCE" => "N",
 		"USE_GIFTS" => "Y",
 		"USE_PREPAYMENT" => "N",
-		"USE_PRICE_ANIMATION" => "Y"
-	)
-);?>
-<?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"basket",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "",
-		"BIG_DATA_RCM_TYPE" => "bestsell",
-		"EDIT_TEMPLATE" => "standard.php",
-		"PATH" => SITE_DIR."include/comp_basket_bigdata.php",
-		"PRICE_CODE" => array("РРЦ 2022","РРЦ Константа"),
-		"SALE_STIKER" => "SALE_TEXT",
-		"SHOW_DISCOUNT_PERCENT_NUMBER" => "N",
-		"STIKERS_PROP" => "HIT",
-		"STORES" => array("","1","2","")
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+		"USE_PRICE_ANIMATION" => "Y",
+		"COMPONENT_TEMPLATE" => "intervolga",
+		"ADDITIONAL_PICT_PROP_27" => "-"
+	),
+	false
+);
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
