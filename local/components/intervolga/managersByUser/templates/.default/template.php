5bb9095d002631947bbce4597ccae4b413d0c2b6
Иван Трутнев <trutnev@intervolga.ru>
2022-09-01 15:14:19 +0300

    [iberisweb-121] выровнял телефоны помощников по центру

--- local/components/intervolga/managersByUser/templates/.default/template.php
+++ local/components/intervolga/managersByUser/templates/.default/template.php
@@ -30,6 +30,12 @@
 						<?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER") ?>
 					<? endif; ?>
 				</th>
+
+				<th>
+					<? if ($partner["UF_POMOSHNIK3"]): ?>
+						<?= \Bitrix\Main\Localization\Loc::getMessage("MANAGER_HELPER") ?>
+					<? endif; ?>
+				</th>
 			</tr>
 
 			<tr>
@@ -37,39 +43,86 @@
 				<td><?= $partner["UF_OSNOVNOYMENEDZHER"] ?></td>
 				<td><?= $partner["UF_POMOSHNIK1"] ?></td>
 				<td><?= $partner["UF_POMOSHNIK2"] ?></td>
+				<td><?= $partner["UF_POMOSHNIK3"] ?></td>
 			</tr>
 
 			<tr>
-				<td>
+				<td class="partner-content">
 					<? if ($partner["UF_IMLOGIN"]): ?>
+						<span>Login: </span>
 						<a href="mailto:<?= $partner["UF_IMLOGIN"] ?>"><?= $partner["UF_IMLOGIN"] ?></a>
 					<? endif; ?>
 				</td>
-				<td>
+				<td class="manager-content">
 					<? if ($partner["UF_OSNMENEDZHERTELEF"]): ?>
+						<span>Тел: </span>
 						<a href="tel:<?= $partner["UF_OSNMENEDZHERTELEF"] ?>"><?= $partner["DISPLAY_OSNMENEDZHERTELEF"] ?></a>
 					<? endif; ?>
 
+					<? if ($partner["UF_OSNMENEDZHERRABTE"]): ?>
+						<br>
+						<span>Раб. тел: </span>
+						<a href="tel:<?= $partner["UF_OSNMENEDZHERRABTE"] ?>"><?= $partner["DISPLAY_OSNMENEDZHERRABTE"] ?></a>
+					<? endif; ?>
+
 					<? if ($partner["UF_OSNMENEDZHERADRES"]): ?>
+						<br>
+						<span>Почта: </span>
 						<a href="mailto:<?= $partner["UF_OSNMENEDZHERADRES"] ?>"><?= $partner["UF_OSNMENEDZHERADRES"] ?></a>
 					<? endif; ?>
 				</td>
-				<td>
+				<td class="assistant1-content">
 					<? if ($partner["UF_POMOSHNIKTELEFON1"]): ?>
+						<span>Тел: </span>
 						<a href="tel:<?= $partner["UF_POMOSHNIKTELEFON1"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON1"] ?></a>
 					<? endif; ?>
 
+					<? if ($partner["UF_POMRABTELEFON1"]): ?>
+						<br>
+						<span>Раб. тел: </span>
+						<a href="tel:<?= $partner["UF_POMRABTELEFON1"] ?>"><?= $partner["DISPLAY_POMRABTELEFON1"] ?></a>
+					<? endif; ?>
+
 					<? if ($partner["UF_POMOSHNIKADRES1"]): ?>
+						<br>
+						<span>Почта: </span>
 						<a href="mailto:<?= $partner["UF_POMOSHNIKADRES1"] ?>"><?= $partner["UF_POMOSHNIKADRES1"] ?></a>
 					<? endif; ?>
 				</td>
-				<td>
+				<td class="assistant2-content">
 					<? if ($partner["UF_POMOSHNIKTELEFON2"]): ?>
+						<span>Тел: </span>
 						<a href="tel:<?= $partner["UF_POMOSHNIKTELEFON2"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON2"] ?></a>
 					<? endif; ?>
 
+					<? if ($partner["UF_POMRABTELEFON2"]): ?>
+						<br>
+						<span>Раб. тел: </span>
+						<a href="tel:<?= $partner["UF_POMRABTELEFON2"] ?>"><?= $partner["DISPLAY_POMRABTELEFON2"] ?></a>
+					<? endif; ?>
+
 					<? if ($partner["UF_POMOSHNIKADRES2"]): ?>
+						<br>
+						<span>Почта: </span>
 						<a href="mailto:<?= $partner["UF_POMOSHNIKADRES2"] ?>"><?= $partner["UF_POMOSHNIKADRES2"] ?></a>
+					<? endif; ?>
+				</td>
+				<td class="assistant3-content">
+					<? if ($partner["UF_POMOSHNIKTELEFON3"]): ?>
+						<span>Тел: </span>
+						<a href="tel:<?= $partner["UF_POMOSHNIKTELEFON3"] ?>"><?= $partner["DISPLAY_POMOSHNIKTELEFON3"] ?></a>
+					<? endif; ?>
+
+					<? if ($partner["UF_POMRABTELEFON3"]): ?>
+						<br>
+						<span>Раб. тел: </span>
+						<a href="tel:<?= $partner["UF_POMRABTELEFON3"] ?>"><?= $partner["DISPLAY_POMRABTELEFON3"] ?></a>
+					<? endif; ?>
+
+					<? if ($partner["UF_POMOSHNIKADRES3"]): ?>
+						<br>
+						<span>Почта: </span>
+						<a href="mailto:<?= $partner["UF_POMOSHNIKADRES3"] ?>"><?= $partner["UF_POMOSHNIKADRES3"] ?></a>
 					<? endif; ?>
 				</td>
 			</tr>
@@ -77,8 +130,6 @@
 			<tr style="height: 20px"></tr>
 
 		<? endforeach; ?>
-
-
 		</tbody>
 	</table>
 <? endif; ?>
