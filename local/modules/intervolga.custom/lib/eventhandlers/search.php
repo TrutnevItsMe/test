9df0b96ca034502a223a662b71ebee9a6696ab95
Иван Трутнев <trutnev@intervolga.ru>
2022-09-19 10:15:37 +0300

    [iberisweb-154] добавил поиск по частям артикула

---
+++ local/modules/intervolga.custom/lib/eventhandlers/search.php
@@ -0,0 +1,35 @@
+<?namespace Intervolga\Custom\EventHandlers;
+
+use Intervolga\Common\Tools\EventHandler;
+
+class Search extends EventHandler{
+
+	const CATALOG_IBLOCK_ID = 17;
+
+	public static function BeforeIndex($arFields)
+	{
+		if ($arFields["MODULE_ID"] == "iblock"
+			&& $arFields["PARAM2"] == self::CATALOG_IBLOCK_ID)
+		{
+			$db_props = \CIBlockElement::GetProperty(                        // Запросим свойства индексируемого элемента
+				$arFields["PARAM2"],         // BLOCK_ID индексируемого свойства
+				$arFields["ITEM_ID"],          // ID индексируемого свойства
+				array("sort" => "asc"),       // Сортировка (можно упустить)
+				Array("CODE"=>"CML2_ARTICLE")); // CODE свойства (в данном случае артикул)
+
+			if($arProp = $db_props->Fetch())
+			{
+				// разобьем артикул по частям
+				$splittedArticle = explode("-", $arProp["VALUE"]);
+
+				for ($i = 0; $i < count($splittedArticle); ++$i)
+				{
+					// Объединяем по частям
+					$sliceArticle= array_slice($splittedArticle, 0, $i);
+					$arFields["TITLE"] .= " ".implode("-", $sliceArticle);
+				}
+			}
+		}
+
+	}
+}
