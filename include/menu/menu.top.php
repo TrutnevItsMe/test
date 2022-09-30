aaee42d20237e54b9a74d20df06743aff7641c81
ivsupport <info@intervolga.ru>
2022-09-30 13:49:48 +0300

    "Stage changes"

--- include/menu/menu.top.php
+++ include/menu/menu.top.php
@@ -8,7 +8,7 @@
 		"COMPONENT_TEMPLATE" => "top1",
 		"COUNT_ITEM" => "6",
 		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
+		"MAX_LEVEL" => "4",
 		"MENU_CACHE_GET_VARS" => array(
 		),
 		"MENU_CACHE_TIME" => "3600000",
