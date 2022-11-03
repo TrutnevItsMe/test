48cdf7392074a0265c3ec6990663da67b6554df9
ivsupport <info@intervolga.ru>
2022-11-03 12:13:43 +0300

    stage

--- bitrix/templates/cezares/page_blocks/page_title_1.php
+++ bitrix/templates/cezares/page_blocks/page_title_1.php
@@ -1,13 +1,17 @@
 <div class="top_inner_block_wrapper maxwidth-theme">
 	<section class="page-top maxwidth-theme <?CNext::ShowPageProps('TITLE_CLASS');?>">
-		<?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "next", array(
-			"START_FROM" => "0",
-			"PATH" => "",
-			"SITE_ID" => SITE_ID,
-			"SHOW_SUBSECTIONS" => "N"
-			),
-			false
-		);?>
+		<?$APPLICATION->IncludeComponent(
+	"bitrix:breadcrumb", 
+	"next1", 
+	array(
+		"START_FROM" => "0",
+		"PATH" => "",
+		"SITE_ID" => "s1",
+		"SHOW_SUBSECTIONS" => "N",
+		"COMPONENT_TEMPLATE" => "next1"
+	),
+	false
+);?>
 		<div class="page-top-main">
 			<?=$APPLICATION->ShowViewContent('product_share')?>
 			<h1 id="pagetitle"><?$APPLICATION->ShowTitle(false)?></h1>
