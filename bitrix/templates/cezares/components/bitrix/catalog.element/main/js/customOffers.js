65f89afbd83a2540fdbbd125acd683f8ccd909ad
Иван Трутнев <trutnev@intervolga.ru>
2022-11-07 11:37:30 +0300

    [iberisweb-148] исправил зависание страницы (исправил бесконечный цикл)

--- bitrix/templates/cezares/components/bitrix/catalog.element/main/js/customOffers.js
+++ bitrix/templates/cezares/components/bitrix/catalog.element/main/js/customOffers.js
@@ -64,16 +64,8 @@ window.OffersFilterComponent = {
 		this.setCharacters();
 		this.setAccessibleFilterItems();
 
-		let prices = {
-			price: 0,
-			old: 0
-		}
-
 		if (this.result["PRICE_MATRIX"])
 		{
-			let currentCurrencyIndex = Object.keys(this.result["PRICE_MATRIX"]["COLS"])[0];
-			prices.price = this.result["PRICE_MATRIX"]["MATRIX"][currentCurrencyIndex][0]["PRICE"];
-
 			if (this.result["CURRENT_OFFER"])
 			{
 				$('.button_block .btn.to-cart').attr("data-item", this.result["CURRENT_OFFER"]["ID"]);
@@ -85,10 +77,6 @@ window.OffersFilterComponent = {
 		if (price.price > 0)
 		{
 			showPrice(price);
-		}
-		else
-		{
-			showPrice(prices);
 		}
 
 		this.bindEvents();
@@ -234,7 +222,9 @@ window.OffersFilterComponent = {
 
 			self.filterProps.forEach(function (propCode)
 			{
-				if (self.result["OFFERS_MAP_FILTER"][prop] && self.result["OFFERS_MAP_FILTER"][prop][value])
+				if (self.result["OFFERS_MAP_FILTER"]
+					&& self.result["OFFERS_MAP_FILTER"][prop]
+					&& self.result["OFFERS_MAP_FILTER"][prop][value])
 				{
 					let ar = [];
 
