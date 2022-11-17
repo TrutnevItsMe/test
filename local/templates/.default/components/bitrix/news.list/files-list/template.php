c81776437427b5c780180370dada719930812dc6
chernikov <chernikov@intervolga.ru>
2022-11-17 15:30:57 +0300

    [iberisweb-158] - Поменял сокращённые php теги на полные

--- local/templates/.default/components/bitrix/news.list/files-list/template.php
+++ local/templates/.default/components/bitrix/news.list/files-list/template.php
@@ -1,6 +1,6 @@
-<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die(); ?>
-<?
+<?php
 
 $this->setFrameMode(true);
 \Bitrix\Main\Page\Asset::getInstance()->addCss($templatePath . "/style.css");
@@ -8,36 +8,36 @@ $this->setFrameMode(true);
 
 
 
-<? if ($arResult['SECTIONS']): ?>
-	<? if ($arParams['SHOW_TITLE'] == 'Y'): ?>
+<?php if ($arResult['SECTIONS']): ?>
+	<?php if ($arParams['SHOW_TITLE'] == 'Y'): ?>
 		<div class="title-tab-heading visible-xs"><?=$arParams["T_TITLE"];?></div>
-	<? endif; ?>
+	<?php endif; ?>
 	<div class="info-row-wrapper item-views <?=$arParams['VIEW_TYPE']?> <?=$arParams['VIEW_TYPE']?>-type-block <?=($arParams['SHOW_TABS'] == 'Y' ? 'with_tabs' : '')?> <?=($arParams['IMAGE_POSITION'] ? 'image_' . $arParams['IMAGE_POSITION'] : '')?> <?=($templateName = $component->{'__parent'}->{'__template'}->{'__name'})?>">
-		<? // top pagination?>
-		<? if ($arParams['DISPLAY_TOP_PAGER']): ?>
+		<?php // top pagination?>
+		<?php if ($arParams['DISPLAY_TOP_PAGER']): ?>
 			<?=$arResult['NAV_STRING']?>
-		<? endif; ?>
+		<?php endif; ?>
 
-		<? // tabs?>
-		<? if ($arParams['SHOW_TABS'] == 'Y'): ?>
+		<?php // tabs?>
+		<?php if ($arParams['SHOW_TABS'] == 'Y'): ?>
 		<div class="tabs">
 			<ul class="nav nav-tabs">
-				<? $i = 0; ?>
-				<? foreach ($arResult['SECTIONS'] as $SID => $arSection): ?>
-					<? if (!$SID)
+				<?php $i = 0; ?>
+				<?php foreach ($arResult['SECTIONS'] as $SID => $arSection): ?>
+					<?php if (!$SID)
 						continue; ?>
 					<li class="<?=$i++ == 0 ? 'active' : ''?>"><a data-toggle="tab"
 																  href="#<?=$this->GetEditAreaId($arSection['ID'])?>"><?=$arSection['NAME']?></a>
 					</li>
-				<? endforeach; ?>
+				<?php endforeach; ?>
 			</ul>
-			<? endif; ?>
+			<?php endif; ?>
 
 			<div class="<?=($arParams['SHOW_TABS'] == 'Y' ? 'tab-content' : 'group-content')?>">
-				<? // group elements by sections?>
-				<? foreach ($arResult['SECTIONS'] as $SID => $arSection): ?>
+				<?php // group elements by sections?>
+				<?php foreach ($arResult['SECTIONS'] as $SID => $arSection): ?>
 
-					<?
+					<?php
 					// edit/add/delete buttons for edit mode
 					$arSectionButtons = CIBlock::GetPanelButtons($arSection['IBLOCK_ID'], 0, $arSection['ID'], [
 						'SESSID' => false,
@@ -49,28 +49,28 @@ $this->setFrameMode(true);
 					<div id="<?=$this->GetEditAreaId($arSection['ID'])?>"
 						 class="tab-pane <?=(!$si++ || !$arSection['ID'] ? 'active' : '')?>">
 
-						<? if ($arParams['SHOW_SECTION_PREVIEW_DESCRIPTION'] == 'Y'): ?>
+						<?php if ($arParams['SHOW_SECTION_PREVIEW_DESCRIPTION'] == 'Y'): ?>
 
-							<? if ($arParams['SHOW_SECTION_NAME'] != 'N'): ?>
-								<? // section name?>
-								<? if (strlen($arSection['NAME'])): ?>
+							<?php if ($arParams['SHOW_SECTION_NAME'] != 'N'): ?>
+								<?php // section name?>
+								<?php if (strlen($arSection['NAME'])): ?>
 									<h3><?=$arSection['NAME']?></h3>
-								<? endif; ?>
-							<? endif; ?>
+								<?php endif; ?>
+							<?php endif; ?>
 
-							<? // section description text/html?>
-							<? if (strlen($arSection['DESCRIPTION']) && strpos($_SERVER['REQUEST_URI'], 'PAGEN') === false): ?>
+							<?php // section description text/html?>
+							<?php if (strlen($arSection['DESCRIPTION']) && strpos($_SERVER['REQUEST_URI'], 'PAGEN') === false): ?>
 								<div class="text_before_items">
 									<?=$arSection['DESCRIPTION']?>
 								</div>
-								<? if ($arParams['SHOW_SECTION_DESC_DIVIDER'] == 'Y'): ?>
+								<?php if ($arParams['SHOW_SECTION_DESC_DIVIDER'] == 'Y'): ?>
 									<hr class="sect-divider" />
-								<? endif; ?>
-							<? endif; ?>
-						<? endif; ?>
+								<?php endif; ?>
+							<?php endif; ?>
+						<?php endif; ?>
 
-						<? foreach ($arSection['ITEMS'] as $i => $arItem): ?>
-							<?
+						<?php foreach ($arSection['ITEMS'] as $i => $arItem): ?>
+							<?php
 							$arParams['SHOW_DETAIL_LINK'] = "Y";
 							// edit/add/delete buttons for edit mode
 							$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_EDIT'));
@@ -79,11 +79,11 @@ $this->setFrameMode(true);
 							$bDetailLink = $arParams['SHOW_DETAIL_LINK'] != 'N' && (!strlen($arItem['DETAIL_TEXT']) ? ($arParams['HIDE_LINK_WHEN_NO_DETAIL'] !== 'Y' && $arParams['HIDE_LINK_WHEN_NO_DETAIL'] != 1) : true);
 							?>
 
-							<? if ($arItem["DISPLAY_PROPERTIES"]["FILE"]): ?>
+							<?php if ($arItem["DISPLAY_PROPERTIES"]["FILE"]): ?>
 								<h4><?=$arItem["NAME"]?></h4>
-								<? $file = $arItem["DISPLAY_PROPERTIES"]["FILE"][0]; ?>
+								<?php $file = $arItem["DISPLAY_PROPERTIES"]["FILE"][0]; ?>
 
-								<?
+								<?php
 								$img = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"], [
 									"width" => 100,
 									"height" => 100
@@ -96,26 +96,26 @@ $this->setFrameMode(true);
 									<div class="info-row__inner">
 										<div class="info-row__item info-row__item_image">
 											<a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
-												<? if ($img): ?>
+												<?php if ($img): ?>
 													<img src="<?=$img["src"]?>"
 														 width="<?=$img["width"]?>"
 														 height="<?=$img["height"]?>" />
-												<? else: ?>
+												<?php else: ?>
 													<img src="<?=SITE_TEMPLATE_PATH?>/images/no_photo_small.png"
 														 width="100"
 														 height="100">
-												<? endif ?>
+												<?php endif ?>
 											</a>
 										</div>
 										<div class="info-row__item info-row__item_name">
 											<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><p><?=$file["ORIGINAL_NAME"]?></p>
 											</a>
 										</div>
-										<? if ($file["TIMESTAMP_X"]): ?>
+										<?php if ($file["TIMESTAMP_X"]): ?>
 											<div class="info-row__item">
 												<p><?=$file["TIMESTAMP_X"]->format("d.m.Y")?></p>
 											</div>
-										<? endif; ?>
+										<?php endif; ?>
 										<div class="info-row__item">
 											<a href="<?=$file["SRC"]?>" download="<?=$file["ORIGINAL_NAME"]?>">
 												<p class="btn btn-info"><?=\Bitrix\Main\Localization\Loc::getMessage("DOWNLOAD")?></p>
@@ -124,20 +124,20 @@ $this->setFrameMode(true);
 									</div>
 								</div>
 
-							<? endif; ?>
+							<?php endif; ?>
 
-						<? endforeach; ?>
+						<?php endforeach; ?>
 					</div>
-				<? endforeach; ?>
+				<?php endforeach; ?>
 			</div>
 
-			<? if ($arParams['SHOW_TABS'] == 'Y'): ?>
+			<?php if ($arParams['SHOW_TABS'] == 'Y'): ?>
 		</div>
-	<? endif; ?>
+	<?php endif; ?>
 
-		<? // bottom pagination?>
-		<? if ($arParams['DISPLAY_BOTTOM_PAGER']): ?>
+		<?php // bottom pagination?>
+		<?php if ($arParams['DISPLAY_BOTTOM_PAGER']): ?>
 			<?=$arResult['NAV_STRING']?>
-		<? endif; ?>
+		<?php endif; ?>
 	</div>
-<? endif; ?>
+<?php endif; ?>
