7c62ac85d5cab398e2506606db115a03f98974d8
Иван Трутнев <trutnev@intervolga.ru>
2022-12-13 10:41:12 +0300

    [iberisweb-238] исправил отображение остатков в детальной товара

--- bitrix/templates/cezares/components/bitrix/catalog.element/main/template.php
+++ bitrix/templates/cezares/components/bitrix/catalog.element/main/template.php
@@ -98,8 +98,8 @@ $strObName = 'ob'.preg_replace("/[^a-zA-Z0-9_]/", "x", $strMainID);
 
 $arResult["strMainID"] = $this->GetEditAreaId($arResult['ID']);
 $arItemIDs=CNext::GetItemsIDs($arResult, "Y");
+$arParams["STORES"] = $arParams["STORES"][1];
 $totalCount = CNext::GetTotalCount($arResult, $arParams);
-
 $arQuantityData = CNext::GetQuantityArray($totalCount, $arItemIDs["ALL_ITEM_IDS"], "Y");
 
 $arParams["BASKET_ITEMS"]=($arParams["BASKET_ITEMS"] ? $arParams["BASKET_ITEMS"] : array());
