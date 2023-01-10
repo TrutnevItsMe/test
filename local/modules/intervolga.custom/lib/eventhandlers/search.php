69beb021f742ad950493803e2aedeb074a16dfa2
Ivan Trutnev <www.vanya22@gmail.com>
2023-01-10 13:16:17 +0300

    [iberisweb-257] исключил из индексирования товары из разделов "Комплектующие для" и "Снято с производства"

--- local/modules/intervolga.custom/lib/eventhandlers/search.php
+++ local/modules/intervolga.custom/lib/eventhandlers/search.php
@@ -1,17 +1,44 @@
 <?
 namespace Intervolga\Custom\EventHandlers;
 
+use Bitrix\Bizproc\Automation\Engine\DelayInterval;
 use Intervolga\Common\Tools\EventHandler;
 
 class Search extends EventHandler
 {
-	const CATALOG_IBLOCK_ID = 17;
-
 	public static function BeforeIndex($arFields)
 	{
+		// Выбираем ID каталога
+		\Bitrix\Main\Loader::includeModule('catalog');
+		$rsCatalogIblock = \Bitrix\Catalog\CatalogIblockTable::getList([
+			'filter' => ['PRODUCT_IBLOCK_ID' => 0]
+		])->fetch();
+
+		$CATALOG_IBLOCK_ID = $rsCatalogIblock["IBLOCK_ID"];
+
 		if ($arFields["MODULE_ID"] == "iblock"
-			&& $arFields["PARAM2"] == self::CATALOG_IBLOCK_ID)
+			&& $arFields["PARAM2"] == $CATALOG_IBLOCK_ID)
 		{
+			\Bitrix\Main\Loader::includeModule("iblock");
+
+			if ($arFields["ITEM_ID"])
+			{
+				// Выбираем раздел товара
+				$elementSection = \CIBlockElement::GetElementGroups($arFields["ITEM_ID"]);
+				$elementSection = $elementSection->Fetch();
+
+				$outOfProductionCode = "snyato_s_proizvodstva";
+				$accessoriesForCodePart = "komplektuyushchie_dlya";
+
+				// Исключаем из поиска товары из разделов "Комплектующие для" и "Снято с производства"
+				if (strstr($elementSection["CODE"], $outOfProductionCode) !== false
+				|| strstr($elementSection["CODE"], $accessoriesForCodePart) !== false)
+				{
+					$arFields = [];
+					return $arFields;
+				}
+			}
+
 			$db_props = \CIBlockElement::GetProperty
 			(                                 // Запросим свойства индексируемого элемента
 				$arFields["PARAM2"],         // BLOCK_ID индексируемого свойства
